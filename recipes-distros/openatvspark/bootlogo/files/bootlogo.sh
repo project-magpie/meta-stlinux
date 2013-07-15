bf18e646212cd907297547e45d5e6e7790f82347
skl <skl@skl-VirtualBox.(none)>
2013-07-15 22:44:58 +0200

    Fix bootlogo.sh

--- recipes-distros/openatvspark/bootlogo/files/bootlogo.sh
+++ recipes-distros/openatvspark/bootlogo/files/bootlogo.sh
@@ -1,4 +1,9 @@
 # thoug not actually a bootlogo task, set the correct videomode before showing the bootlogo
 cat /etc/videomode > /proc/stb/video/videomode
 
-[ -e /etc/dropbear/dropbear_rsa_host_key ] &&  /usr/bin/showiframe /usr/share/bootlogo.mvi || /usr/bin/showiframe /usr/share/bootlogo_wait.mvi
+if [ -e /etc/dropbear/dropbear_rsa_host_key ] ; then
+	/usr/bin/showiframe -p /usr/share/bootlogo.mvi &
+else 
+	/usr/bin/showiframe -p /usr/share/bootlogo_wait.mvi &
+fi
+
